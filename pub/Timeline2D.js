e52a2502d0cc7c925f996eccb1a9f2899d2ab41a
Boyuan Cui <boyuanbryan.cui@mail.utoronto.ca>
2021-03-21 11:21:20 -0400

    Replace eventTime with JS Date
    Also added scale features

--- pub/Timeline2D.js
+++ pub/Timeline2D.js
@@ -1,78 +1,5 @@
 'use strict'
 
-function EventTime(year, month, date, hour, minute, second){
-    this.eventTime = new Date(year, month, date, hour, minute, second);
-    // this.eventTime = [year || -1, month || -1, date || -1, hour || -1, minute || -1, second || -1];
-}
-
-EventTime.prototype = {
-    getValue: function() {
-        const len = this.eventTime.length
-        let value = 0;
-        for (let i = 0; i < this.eventTime.length; i++){
-            if (this.eventTime[i] !== -1){
-                value += this.eventTime[i] * (100 ** (len - i));
-            }
-        }
-        return value;
-    },
-    getFirstDefinedTime: function (){
-        return this.eventTime.getFullYear();
-        for (let i = 0; i < this.eventTime.length; i++){
-            if (this.eventTime[i] !== -1){
-                return this.eventTime[i];
-            }
-        }
-        return undefined;
-    },
-    getTimeString: function() {
-        return this.eventTime.toLocaleDateString();
-        let date = '';
-        for (let i = 0; i < 3; i++){
-            let timeVal = this.eventTime[i];
-            if (timeVal !== -1){
-                if (timeVal < 10 && i != 0) {
-                    timeVal = `0${timeVal}`
-                }
-                date += `${timeVal}-`
-            }
-        }
-        if (this.eventTime[3] !== -1){
-            let time = '';
-            for (let i = 3; i < this.eventTime.length; i++){
-                let timeVal = this.eventTime[i];
-                if (timeVal !== -1){
-                    if (timeVal < 10) {
-                        timeVal = `0${timeVal}`
-                    }
-                    time += `${timeVal}:`
-                } else {
-                    time += '00:'
-                }
-            }
-            return date.slice(0,date.length-1) + ' ' + time.slice(0,time.length-1)
-        }
-        return date.slice(0,date.length-1)
-
-    }
-};
-
-function getInterval (timeA, timeB) {
-    return timeB - timeA;
-}
-
-function valueToEventTime (timeVal) {
-    let times = []
-    for (let i = 6; i > 0; i--){
-        const timePartial = Math.floor(timeVal / 100**i);
-        times.push(timePartial);
-        timeVal -= timePartial * 100 ** i;
-    }
-    let eventTime = new EventTime();
-    eventTime.eventTime = times;
-    return eventTime;
-}
-
 function TimelineEvent (title, description, category, time) {
     this.title = title;
     this.description = description;
@@ -82,7 +9,15 @@ function TimelineEvent (title, description, category, time) {
 
 TimelineEvent.prototype = {
     getTime: function() {
-        return this.time.toLocaleDateString();
+        return this.time;
+    },
+
+    getTimeString: function() {
+        return this.time.toLocaleString();
+    },
+
+    getPrimaryTime: function() {
+        return this.time.getFullYear()
     }
 }
 
@@ -130,11 +65,11 @@ const dotTitlePadding = 10;
 
 Timeline2D.prototype = {
 
-    addTimelineEvent: function(title, description, category, year, month, date, hour, minute, second){
+    addTimelineEvent: function(title, description, category, dateString){
 
-        const time = new Date(year, month, date, hour, minute, second);
+        const time = new Date(dateString);
         const event = new TimelineEvent(title, description, category, time);
-        if (!this.timeMin || time < this.timeMin)){
+        if (!this.timeMin || time < this.timeMin){
             this.timeMin = time;
         }
 
@@ -160,9 +95,9 @@ Timeline2D.prototype = {
         let partialMax = this.renderMin;
         while (partialMax <= this.renderMax){
             scales.push(partialMax);
-            partialMax += this.scaleUnit;
+            partialMax = new Date(partialMax.valueOf() + this.scaleUnit);
         }
-        
+        console.log(scales)
         return scales;
     },
 
@@ -191,14 +126,14 @@ Timeline2D.prototype = {
         // Add each event to the timeline
         this.events.forEach(
             (item) => {
-                if (item.time.getValue() < renderMin || item.time.getValue() > renderMax){
+                if (item.getTime() < renderMin || item.getTime() > renderMax){
                     return;
                 }
 
                 // Calculate where should the event be placed on the time flow
                 // based on its time
                 const total = renderMax - renderMin;
-                const fraction = total ? (item.time - renderMin) / total : 0
+                const fraction = total ? (item.getTime() - renderMin) / total : 0
                 const toLeft = (length - dotWidth) * fraction + startX;
                 
                 // Calculate the position based on its category
@@ -222,7 +157,7 @@ Timeline2D.prototype = {
                 // A hidden event title above (or below) the dot which only become visible when the mouse hovers
                 // over the dot
                 const title = document.createElement('div')
-                title.innerText = `${item.time.getFullYear()}, ${item.title}`
+                title.innerText = `${item.getPrimaryTime()}, ${item.title}`
                 title.classList.add('eventTitle')
 
                 dot.addEventListener('mouseenter', (e) => {
@@ -268,7 +203,7 @@ Timeline2D.prototype = {
                     eventDescription.id = 'eventDescription';
                     eventDescription.style.maxHeight = `${descriptionHeight}px`
 
-                    timeHeader.innerText = item.getTime();
+                    timeHeader.innerText = item.getTimeString();
                     eventTitle.innerHTML = `<center><strong>${item.title}</strong></center>`;
                     eventCat.innerText = `${item.category}`;
                     eventDescription.innerHTML = item.description;
@@ -351,39 +286,36 @@ Timeline2D.prototype = {
         );
 
         // Add time scale on the timeflow arrow
-        // const scaleNum = 5;
-        // this.scaleUnit = this.timeMax / scaleNum;
+        const scaleNum = 5;
+        const scalesLength = this.renderMax - this.renderMin;
+        this.scaleUnit = scalesLength / scaleNum;
 
-        // const scaleWidth = 50;
-        // const scales = this.getTimeScales();
-        // scales.forEach(timeVal =>{
-        //     const scalesLength = this.renderMax - this.renderMin;
-        //     const scaleFraction = scalesLength ? ((timeVal - this.renderMin) / scalesLength) : 0;
+        let scales = this.getTimeScales();
+        scales.forEach(time => {
+            const scaleFraction = scalesLength ? (time - this.renderMin) / scalesLength : 0;
 
-        //     const timeScale = document.createElement('div');
-        //     timeScale.classList.add('timelineScale');
-        //     timeScale.style.height = `${flowWidth}px`;
-        //     timeScale.style.width = `${scaleWidth}px`;
-        //     timeScale.style.left = `${scaleFraction * length + startX}px`;
-        //     timeScale.style.top = `${aboveHeight}px`
-        //     timeScale.style.zIndex = `${this.events.length + 1}`;
+            const scale = document.createElement('div');
+            scale.classList.add('timelineScale');
+            scale.style.left = `${startX + scaleFraction * (length - dotWidth)}px`;
+            scale.style.top = `${aboveHeight}px`;
+            scale.style.zIndex = `${this.events.length + 1}`;
 
-        //     const timeString = valueToEventTime(timeVal).getTimeString();
-        //     timeScale.innerText = timeString;
-        //     timeline.appendChild(timeScale);
-        // });
+            scale.innerText = time.getFullYear();
+            
+            timeline.appendChild(scale);
+        });
 
         // create canvas to draw the timeflow arrow
         const timeflow = document.createElement('canvas');
         timeflow.width = length + arrowLength + startX;
         timeflow.height = arrowWidth;
         timeflow.style.zIndex = `${this.events.length}`;
-        const ctx = timeflow.getContext("2d");
-
+        
         // Fix position of arrow
         timeflow.style.top = `${aboveHeight - (arrowWidth - flowWidth)/2}px`
         
         // Draw arrow
+        const ctx = timeflow.getContext("2d");
         ctx.fillRect(startX, startY, length, flowWidth);
         ctx.beginPath();
         ctx.arc(startX, startY + flowWidth/2, circleRadius, 0, Math.PI * 2);
@@ -403,13 +335,12 @@ Timeline2D.prototype = {
     render: function(rendetAt, length) {
         
         // Parameters for zooming
-        const zoomDepth = 20;
+        const zoomDepth = 10;
 
         const zoomUnit = (this.timeMax - this.timeMin) / zoomDepth;
         this.renderMin = this.timeMin;
         this.renderMax = this.timeMax;
         this.unitTime = zoomUnit;
-
 
         const timeline = this.createZoomedTimeline(length, this.timeMin, this.timeMax);
 
@@ -424,7 +355,6 @@ Timeline2D.prototype = {
         const panel = document.createElement('div');
         panel.classList.add('timelinePanel');
 
-
         // Button to zoom in
         const zoomIn = document.createElement('button');
         zoomIn.classList.add('timelineZoom');
@@ -433,8 +363,8 @@ Timeline2D.prototype = {
         zoomIn.addEventListener('click', e => {
 
             const oldTimeline = wrapper.querySelector('.timeline2d');
-            this.renderMin += zoomUnit/2;
-            this.renderMax -= zoomUnit/2;
+            this.renderMin = new Date(this.renderMin.valueOf() + zoomUnit/2);
+            this.renderMax = new Date(this.renderMax.valueOf() - zoomUnit/2);
             const zoomedTimeline = this.createZoomedTimeline(length, this.renderMin, this.renderMax);
             wrapper.replaceChild(zoomedTimeline, oldTimeline);
 
@@ -444,7 +374,7 @@ Timeline2D.prototype = {
                 zoomIn.disabled = false;
             }
             
-            if ((this.renderMax >= this.timeMax && (this.renderMin <= this.timeMin){
+            if ((this.renderMax >= this.timeMax) && (this.renderMin <= this.timeMin)){
                 zoomOut.disabled = true;
             } else {
                 zoomOut.disabled = false;
@@ -463,10 +393,10 @@ Timeline2D.prototype = {
 
             const oldTimeline = wrapper.querySelector('.timeline2d');
             if (this.renderMin > this.timeMin){
-                this.renderMin -= zoomUnit/2;
+                this.renderMin = new Date(this.renderMin.valueOf() - zoomUnit/2);
             }
             if (this.renderMax < this.timeMax){
-                this.renderMax += zoomUnit/2;
+                this.renderMax = new Date(this.renderMax.valueOf() + zoomUnit/2);
             }
             const zoomedTimeline = this.createZoomedTimeline(length, this.renderMin, this.renderMax);
             wrapper.replaceChild(zoomedTimeline, oldTimeline);
@@ -497,8 +427,8 @@ Timeline2D.prototype = {
         prev.addEventListener('click', e => {
             const oldTimeline = wrapper.querySelector('.timeline2d');
             if (this.renderMin > this.timeMin){
-                this.renderMin -= this.unitTime;
-                this.renderMax -= this.unitTime;
+                this.renderMin = new Date(this.renderMin.valueOf() - this.unitTime);
+                this.renderMax = new Date(this.renderMax.valueOf() - this.unitTime);
             }
             const zoomedTimeline = this.createZoomedTimeline(length, this.renderMin, this.renderMax);
             wrapper.replaceChild(zoomedTimeline, oldTimeline)
@@ -513,8 +443,8 @@ Timeline2D.prototype = {
         next.addEventListener('click', e => {
             const oldTimeline = wrapper.querySelector('.timeline2d');
             if (this.renderMax < this.timeMax){
-                this.renderMin += this.unitTime;
-                this.renderMax += this.unitTime;
+                this.renderMin = new Date(this.renderMin.valueOf() + this.unitTime);
+                this.renderMax = new Date(this.renderMax.valueOf() + this.unitTime);
             }
             const zoomedTimeline = this.createZoomedTimeline(length, this.renderMin, this.renderMax);
             wrapper.replaceChild(zoomedTimeline, oldTimeline)
--- pub/example.js
+++ pub/example.js
@@ -2,26 +2,26 @@
 const tl = new Timeline2D();
 
 const jobs = 'Steve Jobs'
-tl.addTimelineEvent('Birth', 'Born in San Francisco, California', jobs, 1955, 2, 24);
-tl.addTimelineEvent('College', 'Enrolled at Reed College', jobs, 1972, 9);
-tl.addTimelineEvent('Drop out', 'Dropped out after one semester', jobs, 1973, 1);
-tl.addTimelineEvent('Founded Apple', 'Co-founded with Steve Wozniak and Ronald Wayne in Cupertino, California', jobs, 1976, 4);
-tl.addTimelineEvent('Death', 'Died at Palo Alto, California', jobs, 2011, 10, 5, 15);
+tl.addTimelineEvent('Birth', 'Born in San Francisco, California', jobs, '1955-02-24');
+tl.addTimelineEvent('College', 'Enrolled at Reed College', jobs, '1972-9');
+tl.addTimelineEvent('Drop out', 'Dropped out after one semester', jobs, '1973-1');
+tl.addTimelineEvent('Founded Apple', 'Co-founded with Steve Wozniak and Ronald Wayne in Cupertino, California', jobs, '1976-4');
+tl.addTimelineEvent('Death', 'Died at Palo Alto, California', jobs, '2011-10-5 15:00');
 
 const gates = 'Bill Gates'
-tl.addTimelineEvent('Birth', 'Born in Seattle, Washington. I have no idea what im d=talking this is nonsensensdfsdjkhfwaeuhisdh s   hasdfhsaihfs i want to mashdufhsidfsdc', gates, 1955, 10, 28)
-tl.addTimelineEvent('College', 'Enrolled at Harvard in the autumn of 1973 with an SAT score of 1590', gates, 1973, 9)
-tl.addTimelineEvent('Drop out', 'Dropped out of Harvard after two years', gates, 1975)
-tl.addTimelineEvent('Founded Microsoft', 'Co-founded with Paul Allen in Albuquerque, New Mexico', gates, 1975, 4)
-tl.addTimelineEvent('Leaving', 'Leaving day-to-day operations of Microsoft', gates, 2008);
+tl.addTimelineEvent('Birth', 'Born in Seattle, Washington. I have no idea what im d=talking this is nonsensensdfsdjkhfwaeuhisdh s   hasdfhsaihfs i want to mashdufhsidfsdc', gates, '1955-10-28')
+tl.addTimelineEvent('College', 'Enrolled at Harvard in the autumn of 1973 with an SAT score of 1590', gates, '1973-9')
+tl.addTimelineEvent('Drop out', 'Dropped out of Harvard after two years', gates, '1975')
+tl.addTimelineEvent('Founded Microsoft', 'Co-founded with Paul Allen in Albuquerque, New Mexico', gates, '1975-4')
+tl.addTimelineEvent('Leaving', 'Leaving day-to-day operations of Microsoft', gates, '2008');
 
 const musk = 'Elon Musk'
-tl.addTimelineEvent('Birth', 'Born in Pretoria, South Africa', musk, 1971, 6, 28)
-tl.addTimelineEvent('College', 'Transferred to University of Pennsylvania',musk, 1992)
-tl.addTimelineEvent('Graduate', 'Graduated with BS in economics and BA in physics', musk, 1997)
-tl.addTimelineEvent('Founded SpaceX','Founded in Hawthorn, California', musk, 2002, 5, 6)
-tl.addTimelineEvent('Founded Tesladfgdfgerdfgsdfgfsd','Founded in San Carlos, California',musk,2003,7,1)
-tl.addTimelineEvent('Founded Neuralink', 'Co-founded in San Francisco',musk, 2016,7)
+tl.addTimelineEvent('Birth', 'Born in Pretoria, South Africa', musk, '1971-6-28')
+tl.addTimelineEvent('College', 'Transferred to University of Pennsylvania',musk, '1992')
+tl.addTimelineEvent('Graduate', 'Graduated with BS in economics and BA in physics', musk, '1997')
+tl.addTimelineEvent('Founded SpaceX','Founded in Hawthorn, California', musk, '2002-5-6')
+tl.addTimelineEvent('Founded Tesladfgdfgerdfgsdfgfsd','Founded in San Carlos, California',musk,'2003-7-1')
+tl.addTimelineEvent('Founded Neuralink', 'Co-founded in San Francisco',musk, '2016-7')
 
 tl.render('body', 800);
 
